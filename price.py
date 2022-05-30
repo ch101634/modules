f405e68ed693b23270ca75489058d80a8554f95d
ch101634 <103934838+ch101634@users.noreply.github.com>
2022-05-31 01:57:13 +0900

    Add files via upload

---
+++ price.py
@@ -0,0 +1,26 @@
+def shopping(shop_file):
+    shop_dict = {} # 생성할 사전 객체
+    with open('data/'+shop_file) as f:
+        for line in f:
+            try:
+                name, price = line.strip().split()
+                price=int(price.replace('원', ''))
+            except:
+                pass
+            if type(price)!=str:
+                shop_dict[name] = price
+    return shop_dict
+
+
+def item_price(shop_file, item):
+    shop_dict = {} # 생성할 사전 객체
+    with open('data/'+shop_file) as f:
+        for line in f:
+            try:
+                name, price = line.strip().split()
+                price=int(price.replace('원', ''))
+            except:
+                pass
+            if type(price)!=str:
+                shop_dict[name] = price
+    return shop_dict[item]
